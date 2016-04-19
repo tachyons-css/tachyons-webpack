9005aa625d7ae4d2fb562f43a5c85a15902d163e
Garren Smith <garren.smith@gmail.com>
2016-04-19 09:37:02 +0200

    Add support for svg, fonts, png and gif

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,34 @@ module.exports = {
       {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.woff2(\?\S*)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff2&name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-tff&name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file?name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.png(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file?name=img/[name].[ext]'
+      },
+      {
+        test: /\.gif(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file?name=img/[name].[ext]'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml&name=img/[name].[ext]'
       }
     ]
   },
